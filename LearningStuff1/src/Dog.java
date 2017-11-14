d49a060ccc51bb08697b25f3e1cb50f058f07ab0
Shoshana Sokolic <shanasok@yahoo.com>
2017-11-13 23:39:52 -0500

    added dog name

--- LearningStuff1/src/Dog.java
+++ LearningStuff1/src/Dog.java
@@ -3,4 +3,24 @@ public class Dog extends Mammal{
 
 	String bark;
 	
+	String name;
+
+	public String getBark() {
+		return bark;
+	}
+
+	public void setBark(String bark) {
+		this.bark = bark;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 }
